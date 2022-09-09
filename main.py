baecca42d2e4e646f42d3d38b1fde4c13112aef4
tom8124 <tom.milla8124@gmail.com>
2022-09-08 21:21:56 -0400

    - Added .gitignore - Finished Count by Year - Added docstrings

--- main.py
+++ main.py
@@ -1,34 +1,96 @@
+"""GroundSpeed Take Home Coding Assignment
+
+Project Scope:
+Companies purchase insurance policies for their fleet of motor vehicles. They must maintain an
+inventory of the fleet along with aggregate and policy information. In this problem you will design
+a data structure to hold the information for cars and some aggregate metrics for the fleet.
+
+1. Design a data structure for a single car that
+- Has a unique id
+- Contains the properties
+- Year, make, model, purchase value, VIN, license plate state, and number
+
+2. Design a data structure that
+- Contains aggregated metrics for the fleet
+- Total count
+- Count by year
+- Total purchase price
+
+3. Write a method that
+- Take a list of car data structures
+- Return an aggregated data structure
+
+4. Design a data structure/method to provide quick lookups for automobiles by VIN.
+
+5. How would you design a data structure/method to provide sorted lists of car data
+structures by property field?
+
+I would use .sort_values() in pandas to sort the data based on values in the column header similar to how you would
+sort a spreadsheet.
+
+Example:
+    fleet.sort_values(by="year", ascending=False)
+
+"""
+import re
 import pandas as pd
 
-car_1 = {
-    'id':['0001'],
-    'year':['2000'],
-    'make':['Kia'],
-    'model':['Sephia'],
-    'purchase value ($)':[850],
-    'VIN':['10FC03IE1994GIWOK'],
-    'license plate state':['NY'],
-    'license plate number':['IWSB012']
-    }
 
-car_2 = {
-    'id':['0002'],
-    'year':['2015'],
-    'make':['RAM'],
-    'model':['2500'],
-    'purchase value ($)':[23000],
-    'VIN':['02TI15WID2022BDWRN'],
-    'license plate state':['NY'],
-    'license plate number':['BTMLT22']
-}
+def vin_lookup(vin, fleet):
+    """This function checks the dataframe of the fleet for a specific VIN number and returns metrics about that vehicle.
 
-fleet = pd.concat([(pd.DataFrame(car_1)),(pd.DataFrame(car_2))])
+    Args:
+        fleet: Dataframe read from data.csv.
+        vin: This parameter is used to check the dataframe for a specific match.
 
-print(pd.DataFrame(fleet))
+    Returns:
+        
+        This function will return single_car which contains a dataframe with metrics for a single car that is found
+        based on a matching VIN record.
 
-metrics = {
-    'Total count':[fleet['id'].count()],
-    'Total purchase price':[fleet['purchase value ($)'].aggregate('sum')]
-    }
+    Raises: An error of "Invalid VIN. Please try again!" will be raised if the user enters something that is not a
+    valid VIN number according to the VIN number format standards.
 
-print(pd.DataFrame(metrics))
+    """
+
+    if not re.match(r'\b[(A-H|J-N|P|R-Z|0-9)]{17}\b', vin):
+        print("Invalid VIN. Please try again!")
+    else:
+        single_car = fleet.loc[fleet['VIN'] == vin]
+        print(single_car)
+        return single_car
+
+
+def main():
+    """This is the main function that holds the rest of the code for the project. This is going to read our fleet data
+    from the data.csv then print that data to show aggregated metrics for the entire fleet. This will then pull the
+    metrics as defined in the project scope and print that dataframe as well. Next it will look for unique years in the
+    fleet dataframe and pull metrics (count and total purchase price by year) and print those as well. Finally we will
+    lookup the vin for a specific vehicle in the fleet dataframe and print the dataframe with metrics for that specific
+    vehicle result.
+
+    """
+
+    fleet = pd.read_csv('data.csv', header=0)
+    print(fleet.sort_values(by="year", ascending=False))
+    metrics = pd.DataFrame({
+        'Total count': [fleet['id'].count()],
+        'Total purchase price': [fleet['purchase value ($)'].aggregate('sum')]
+    })
+    print(metrics)
+    unique_years = fleet.year.unique()
+    count_by_year = pd.DataFrame()
+
+    for year in unique_years:
+        rslt_df = fleet.loc[fleet['year'] == year]
+        new_df_second = pd.DataFrame(
+            {"year": [year], "count": [len(rslt_df)], "purchase value ($)": [sum(rslt_df['purchase value ($)'])]})
+        count_by_year = pd.concat([count_by_year, new_df_second])
+
+    print(count_by_year)
+    vin = input('Please enter the VIN you are looking for: ')
+    vin_lookup(vin, fleet)
+
+
+if __name__ == "__main__":
+    main()
